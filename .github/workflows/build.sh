37f4c848c3ac6d4c66a3925a91ce81d5f8a8542d
Matthew Brett <matthew.brett@gmail.com>
2023-11-08 22:25:34 +0000

    Try Pillow wheel-building machinery

---
+++ .github/workflows/build.sh
@@ -0,0 +1,37 @@
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  # curl from brew requires zstd, use system curl
+  # if php is installed, brew tries to reinstall these after installing openblas
+  brew remove --ignore-dependencies curl php
+
+  brew install pkg-config
+
+  if [[ "$PLAT" == "arm64" ]]; then
+    export MACOSX_DEPLOYMENT_TARGET="11.0"
+  else
+    export MACOSX_DEPLOYMENT_TARGET="10.10"
+  fi
+fi
+
+if [[ "$MB_PYTHON_VERSION" == pypy3* ]]; then
+  MB_PYTHON_OSX_VER="10.9"
+fi
+
+echo "::group::Install a virtualenv"
+  source multibuild/common_utils.sh
+  source multibuild/travis_steps.sh
+  python3 -m pip install virtualenv
+  before_install
+echo "::endgroup::"
+
+echo "::group::Build wheel"
+  clean_code
+  build_wheel
+  ls -l "${GITHUB_WORKSPACE}/${WHEEL_SDIR}/"
+echo "::endgroup::"
+
+if [[ $MACOSX_DEPLOYMENT_TARGET != "11.0" ]]; then
+  echo "::group::Test wheel"
+    install_run
+  echo "::endgroup::"
+fi
--- config.sh
+++ config.sh
@@ -7,21 +7,6 @@ function pre_build {
     # Runs in the root directory of this repository.
     # Workaround for Accelerate error; only on macOS.
     if [ -z "$IS_MACOS" ]; then return; fi
-    # We need NP_BUILD_DEP to know which Numpy we will
-    # get - so this variable must be defined here.
-    # It will be for macOS builds that see the env vars
-    # from the travis config.
-    if [ -z "$NP_BUILD_DEP" ]; then return; fi
-    local np_ver=$(echo $NP_BUILD_DEP | sed -E 's/^numpy[=<>! ]+([0-9.]+)/\1/')
-    if ! [[ $np_ver =~ ^[0-9.]+$ ]]; then
-        echo "Could not parse NP_BUILD_DEP=$NP_BUILD_DEP"
-        exit 1
-    fi
-    # Problem only arises for Numpy >= 1.20
-    if [ $(lex_ver $np_ver) -lt $(lex_ver 1.20) ]; then
-        return
-    fi
-    # Just in case.
     brew install openblas
 }
 
