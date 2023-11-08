f55e8913c119d25cf5af46c911b4a7acfbedea69
Matthew Brett <matthew.brett@gmail.com>
2023-11-08 23:54:44 +0000

    Fixes for arch flags and unusable architecture

--- config.sh
+++ config.sh
@@ -1,12 +1,22 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_MACOS" ]
 
+function clean_arch_flags {
+    # Clean up potential duplicate arch flags
+    if [[ $ARCH_FLAGS =~ "-arch arm64" ]]; then
+        export ARCH_FLAGS="-arch arm64"
+    elif [[ $ARCH_FLAGS =~ "-arch x86_64" ]]; then
+        export ARCH_FLAGS="-arch x86_64"
+    fi
+}
+
 function pre_build {
     # Any stuff that you need to do before you start
     # building the wheels.
     # Runs in the root directory of this repository.
     # Workaround for Accelerate error; only on macOS.
     if [ -z "$IS_MACOS" ]; then return; fi
+    clean_arch_flags
     brew install openblas
 }
 
