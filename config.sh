f8254f6d5433e8b5a05a705a214e256ed0b85b5d
Matthew Brett <matthew.brett@gmail.com>
2023-11-09 11:59:41 +0000

    Moved archflags fix into multibuild

--- config.sh
+++ config.sh
@@ -1,15 +1,6 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_MACOS" ]
 
-function clean_archflags {
-    # Clean up potential duplicate arch flags
-    if [[ $ARCHFLAGS =~ "-arch arm64" ]]; then
-        export ARCHFLAGS="-arch arm64"
-    elif [[ $ARCHFLAGS =~ "-arch x86_64" ]]; then
-        export ARCHFLAGS="-arch x86_64"
-    fi
-}
-
 function pre_build {
     # Any stuff that you need to do before you start
     # building the wheels.
@@ -17,13 +8,6 @@ function pre_build {
     # Workaround for Accelerate error; only on macOS.
     if [ -z "$IS_MACOS" ]; then return; fi
     brew install openblas
-}
-
-
-function pip_wheel_cmd {
-    local abs_wheelhouse=$1
-    clean_archflags
-    pip wheel $(pip_opts) -w $abs_wheelhouse --no-deps .
 }
 
 function run_tests {
