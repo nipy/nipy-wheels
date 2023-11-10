2feb4ddd9721f018922e35cfb6112466e4917054
Matthew Brett <matthew.brett@gmail.com>
2023-11-10 17:47:29 +0000

    Virtualenv python needs tomli

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -16,13 +16,14 @@ fi
 echo "::group::Install a virtualenv"
   source multibuild/common_utils.sh
   source multibuild/travis_steps.sh
-  python3 -m pip install virtualenv tomli
+  python3 -m pip install virtualenv
   before_install
 echo "::endgroup::"
 
 echo "::group::Build wheel"
   export WHEEL_SDIR=wheelhouse
-  export BUILD_DEPENDS=$(python3 ./print_deps.py ${MB_PYTHON_VERSION} ${REPO_DIR})
+  python -m pip install tomli
+  export BUILD_DEPENDS=$(python ./print_deps.py ${MB_PYTHON_VERSION} ${REPO_DIR})
   clean_code
   build_wheel
   ls -l "${GITHUB_WORKSPACE}/${WHEEL_SDIR}/"
